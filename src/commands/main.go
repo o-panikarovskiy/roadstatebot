297a588c16fa2aa2f1dc1be1e7810a2c4c2dae8a
Oleg Panikarovskiy <oleg.pnk@gmail.com>
2020-06-17 20:59:25 +0300

    some stuff

--- src/commands/main.go
+++ src/commands/main.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"crypto/rand"
+	"math/big"
 	"regexp"
 	"roadstatebot/src/bot"
 	"roadstatebot/src/config"
@@ -19,7 +21,36 @@ func Init(ibot bot.IBot, cfg *config.AppConfig, rep repository.IRepository) {
 	ibot.On("/list@"+botName, CountriesList(rep))
 	ibot.OnAnswer("/highways", HighwayListAnswer(rep))
 
-	regHighway, _ := regexp.Compile(`(?i)^/(ua|ru|by|uа)([MPAHМРАН])[0-9]+$`)
-	ibot.OnRegexp(regHighway, FeedbacksList(rep))
+	ibot.OnRegexp(regexp.MustCompile(`(?i)^/(ua|ru|by|uа)([MPAHМРАН])[0-9]+$`), FeedbacksList(rep))
 	ibot.OnAnswer("/feedback", FeedbackAnswer(rep))
+
+	initStuff(ibot)
+}
+
+func initStuff(ibot bot.IBot) {
+	ibot.OnRegexp(regexp.MustCompile(`(?i)^да$`), func(*bot.User, *bot.Chat, *bot.Message) *bot.Message {
+		text := "пизда"
+
+		b, err := rand.Int(rand.Reader, big.NewInt(1000))
+		if err == nil && b.Int64()%2 == 0 {
+			text = "хуй на"
+		}
+
+		return &bot.Message{Text: text}
+	})
+
+	ibot.OnRegexp(regexp.MustCompile(`(?i)^нет$`), func(*bot.User, *bot.Chat, *bot.Message) *bot.Message {
+		text := "пидара ответ"
+
+		b, err := rand.Int(rand.Reader, big.NewInt(1000))
+		if err == nil && b.Int64()%2 == 0 {
+			text = "шлюхи аргумент"
+		}
+
+		return &bot.Message{Text: text}
+	})
+
+	ibot.OnRegexp(regexp.MustCompile(`(?i)^(макс|максим|максимка|антон|тоха|виталий|веталь|витаха|олег|олежа)$`), func(*bot.User, *bot.Chat, *bot.Message) *bot.Message {
+		return &bot.Message{Text: "ты красавчик"}
+	})
 }
